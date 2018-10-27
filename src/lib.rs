bdf44acb62a2e9870912636dfd376e0b10834923
Jeremy Rubin <j@rubin.io>
2018-10-27 02:30:56 -0700

    Switch to Knuth's Algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,32 @@ fn prior_power_of_two(x: usize) -> usize {
     ((x as u128).next_power_of_two() >> 1) as usize
 }
 
+// Algorithm From Knuth's Sorting and Searching 5.2.2M
+pub fn const_sort<T, F>(v: &mut [T], cmp: &F)
+where
+    F: Fn(&T, &T) -> subtle::Choice,
+    T: subtle::ConditionallySwappable,
+{
+    let t = prior_power_of_two(v.len());
+    let mut p = t;
+    while p > 0 {
+        let mut q = t;
+        let mut r = 0;
+        let mut d = p;
+        while d > 0 {
+            for i in 0..(v.len()-d) {
+                if (i & p) == r {
+                    let (f,l) = v.split_at_mut(1+i);
+                    compare_and_swap(&mut l[d-1], &mut f[i], cmp);
+                }
+            }
+            d = q -p;
+            q >>=1;
+            r = p;
+        }
+        p >>=1;
+    }
+}
 #[inline(always)]
 fn compare_and_swap<T, F>(v: &mut T, s: &mut T, cmp: &F)
 where
@@ -18,107 +44,6 @@ where
     v.conditional_swap(s, choice);
 }
 
-#[inline(always)]
-pub fn const_sort<T, F>(v: &mut [T], cmp: &F)
-where
-    F: Fn(&T, &T) -> subtle::Choice,
-    T: subtle::ConditionallySwappable,
-{
-    const_sort_asc(v, cmp);
-}
-
-pub fn const_sort_desc<T, F>(v: &mut [T], cmp: &F)
-where
-    F: Fn(&T, &T) -> subtle::Choice,
-    T: subtle::ConditionallySwappable,
-{
-    match v.len() {
-        0 | 1 => {
-            return;
-        }
-        2 => {
-            let (first, last) = v.split_at_mut(1);
-            compare_and_swap(&mut first[0], &mut last[0], cmp)
-        }
-        n => {
-            let m = n.wrapping_shr(1);
-            const_sort_asc(&mut v[0..m], cmp);
-            const_sort_desc(&mut v[m..n], cmp);
-            const_merge_desc(v, cmp);
-        }
-    }
-}
-
-pub fn const_sort_asc<T, F>(v: &mut [T], cmp: &F)
-where
-    F: Fn(&T, &T) -> subtle::Choice,
-    T: subtle::ConditionallySwappable,
-{
-    match v.len() {
-        0 | 1 => {
-            return;
-        }
-        2 => {
-            let (first, last) = v.split_at_mut(1);
-            compare_and_swap(&mut last[0], &mut first[0], cmp)
-        }
-        n => {
-            let m = n.wrapping_shr(1);
-            const_sort_desc(&mut v[0..m], cmp);
-            const_sort_asc(&mut v[m..n], cmp);
-            const_merge_asc(v, cmp);
-        }
-    }
-}
-
-fn const_merge_asc<T, F>(v: &mut [T], cmp: &F)
-where
-    F: Fn(&T, &T) -> subtle::Choice,
-    T: subtle::ConditionallySwappable,
-{
-    let n = v.len();
-    if n <= 1 {
-        return;
-    }
-    let m = prior_power_of_two(n);
-
-    // We want to compare 0 to n-m and m to n OR 0 to m and m to n. Either way
-    // we are comparing (n-m) to m of th elements. It's easier to do 0 to m and
-    // m to n expressed in rust.
-    let (left, right) = v.split_at_mut(m);
-    // N.B., technically the right side is guaranteed to be of size (n -m)
-    // and the left size m.
-    //
-    // 2m <= n as a consequence of m being the prior power of two
-    // 2m -m <= n-m
-    // m <= n-m
-    //
-    // Therefore, every element on the right side is iterated over but we
-    // may not compare elements in the middle.
-    //
-    left.iter_mut()
-        .zip(right.iter_mut())
-        .for_each(|(l, r)| compare_and_swap(r, l, cmp));
-    const_merge_asc(&mut v[0..m], cmp);
-    const_merge_asc(&mut v[m..n], cmp);
-}
-fn const_merge_desc<T, F>(v: &mut [T], cmp: &F)
-where
-    F: Fn(&T, &T) -> subtle::Choice,
-    T: subtle::ConditionallySwappable,
-{
-    let n = v.len();
-    if n <= 1 {
-        return;
-    }
-    let m = prior_power_of_two(n);
-    let (left, right) = v.split_at_mut(m);
-    left.iter_mut()
-        .zip(right.iter_mut())
-        .for_each(|(l, r)| compare_and_swap(l, r, cmp));
-    const_merge_desc(&mut v[0..m], cmp);
-    const_merge_desc(&mut v[m..n], cmp);
-}
 
 #[cfg(test)]
 mod tests {
